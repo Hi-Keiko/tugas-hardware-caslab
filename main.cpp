90738ff031fd63f5412f6924b116defa63d515f1
fathihrf <fathih.firmansyah@gmail.com>
2025-11-06 10:15:28 +0700

    Refactor MPU6050 integration and vibration handling
    Updated MPU6050 library and adjusted initialization. Refactored vibration magnitude calculations and LCD display logic.

--- main.cpp
+++ main.cpp
@@ -1,83 +1,98 @@
+#include <Arduino.h>
 #include <Wire.h>
-#include <MPU6050.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 #include <LiquidCrystal_I2C.h>
 
-// Buat dua instance I2C
-TwoWire I2C_MPU = TwoWire(0);   // I2C channel 0 untuk MPU6050
-TwoWire I2C_LCD = TwoWire(1);   // I2C channel 1 untuk LCD
+// Buat dua bus I2C
+TwoWire I2C_MPU = TwoWire(0);   // I2C untuk MPU6050
+TwoWire I2C_LCD = TwoWire(1);   // I2C untuk LCD
 
-MPU6050 mpu(&I2C_MPU);
-LiquidCrystal_I2C lcd(0x27, 16, 2); // Ganti 0x27 jika alamat berbeda
+Adafruit_MPU6050 mpu;
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Ganti 0x27 jika LCD kamu beda alamat
 
 const int buzzerPin = 13;
-
-float accelX, accelY, accelZ;
-float vibrationMagnitude = 0;
-float vibrationThreshold = 0.15;  // semakin kecil = semakin sensitif
+float vibrationThreshold = 0.15; // semakin kecil semakin sensitif
 
 void setup() {
   Serial.begin(115200);
 
-  // --- Inisialisasi I2C untuk masing-masing perangkat ---
-  I2C_MPU.begin(21, 22);   // SDA, SCL default untuk MPU6050
-  I2C_LCD.begin(16, 17);   // SDA=16, SCL=17 (custom pin untuk LCD)
+  // Inisialisasi dua bus I2C
+  I2C_MPU.begin(21, 22); // SDA, SCL untuk MPU6050
+  I2C_LCD.begin(16, 17); // SDA, SCL untuk LCD (port digital custom)
 
-  // --- Inisialisasi MPU6050 ---
-  mpu.initialize();
-  if (!mpu.testConnection()) {
-    Serial.println("MPU6050 tidak terdeteksi!");
-    while (1);
-  }
-
-  // --- Inisialisasi LCD ---
-  lcd.begin(16, 2);  // penting untuk inisialisasi internal
+  // Inisialisasi LCD
+  lcd.begin(16, 2);
   lcd.init();
   lcd.backlight();
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Seismograf ESP32");
+  delay(1500);
+  lcd.clear();
 
-  // --- Inisialisasi buzzer ---
+  // Inisialisasi MPU6050
+  if (!mpu.begin(0x68, &I2C_MPU)) {
+    Serial.println("MPU6050 tidak terdeteksi!");
+    lcd.setCursor(0, 0);
+    lcd.print("MPU ERROR!");
+    while (1);
+  }
+
+  // Atur rentang sensitivitas
+  mpu.setAccelerometerRange(MPU6050_RANGE_2_G); // paling sensitif
+  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+  // Inisialisasi buzzer
   pinMode(buzzerPin, OUTPUT);
   digitalWrite(buzzerPin, LOW);
 
-  delay(2000);
+  Serial.println("MPU6050 Siap!");
+  lcd.setCursor(0, 0);
+  lcd.print("MPU6050 Siap!");
+  delay(1000);
   lcd.clear();
 }
 
 void loop() {
-  // --- Baca data akselerometer dari MPU6050 ---
-  accelX = mpu.getAccelerationX() / 16384.0;
-  accelY = mpu.getAccelerationY() / 16384.0;
-  accelZ = mpu.getAccelerationZ() / 16384.0;
+  sensors_event_t accel, gyro, temp;
+  mpu.getEvent(&accel, &gyro, &temp);
 
-  // --- Hitung magnitude getaran ---
-  vibrationMagnitude = sqrt(accelX * accelX + accelY * accelY + accelZ * accelZ) - 1.0;
-  if (vibrationMagnitude < 0) vibrationMagnitude = -vibrationMagnitude;
+  // Hitung magnitudo getaran (tanpa arah)
+  float magnitude = sqrt(accel.acceleration.x * accel.acceleration.x +
+                         accel.acceleration.y * accel.acceleration.y +
+                         accel.acceleration.z * accel.acceleration.z) - 9.81;
 
-  // --- Tampilkan hasil di Serial ---
+  // Konversi ke nilai absolut (m/s²)
+  if (magnitude < 0) magnitude = -magnitude;
+
+  // Konversi ke G-force
+  float magnitude_g = magnitude / 9.81;
+
+  // Cetak ke Serial Monitor
   Serial.print("Getaran: ");
-  Serial.print(vibrationMagnitude, 3);
+  Serial.print(magnitude_g, 3);
   Serial.println(" g");
 
-  // --- Tampilkan di LCD ---
+  // Tampilkan ke LCD
   lcd.setCursor(0, 0);
   lcd.print("Getaran: ");
-  lcd.print(vibrationMagnitude, 2);
+  lcd.print(magnitude_g, 2);
   lcd.print("g   ");
 
   lcd.setCursor(0, 1);
-  if (vibrationMagnitude < vibrationThreshold)
+  if (magnitude_g < vibrationThreshold)
     lcd.print("Tenang          ");
-  else if (vibrationMagnitude < 0.4)
+  else if (magnitude_g < 0.4)
     lcd.print("Getaran Ringan  ");
-  else if (vibrationMagnitude < 0.8)
+  else if (magnitude_g < 0.8)
     lcd.print("Getaran Sedang  ");
   else
     lcd.print("Getaran Kuat!!! ");
 
-  // --- Kontrol buzzer ---
-  if (vibrationMagnitude > 0.6)
+  // Kontrol buzzer
+  if (magnitude_g > 0.6)
     digitalWrite(buzzerPin, HIGH);
   else
     digitalWrite(buzzerPin, LOW);

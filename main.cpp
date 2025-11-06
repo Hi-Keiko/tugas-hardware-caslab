fbf8942c866279089881503ac435d7a3d960a9ec
fathihrf <fathih.firmansyah@gmail.com>
2025-11-06 10:22:34 +0700

    Refactor I2C initialization and simplify code

--- main.cpp
+++ main.cpp
@@ -1,25 +1,16 @@
-#include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <LiquidCrystal_I2C.h>
 
-// Buat dua bus I2C
-TwoWire I2C_MPU = TwoWire(0);   // I2C untuk MPU6050
-TwoWire I2C_LCD = TwoWire(1);   // I2C untuk LCD
-
 Adafruit_MPU6050 mpu;
-LiquidCrystal_I2C lcd(0x27, 16, 2); // Ganti 0x27 jika LCD kamu beda alamat
-
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // Ubah alamat jika perlu
 const int buzzerPin = 13;
-float vibrationThreshold = 0.15; // semakin kecil semakin sensitif
+float vibrationThreshold = 0.15;
 
 void setup() {
   Serial.begin(115200);
-
-  // Inisialisasi dua bus I2C
-  I2C_MPU.begin(21, 22); // SDA, SCL untuk MPU6050
-  I2C_LCD.begin(16, 17); // SDA, SCL untuk LCD (port digital custom)
+  Wire.begin(21, 22); // Satu bus I2C
 
   // Inisialisasi LCD
   lcd.begin(16, 2);
@@ -28,30 +19,23 @@ void setup() {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Seismograf ESP32");
-  delay(1500);
-  lcd.clear();
 
   // Inisialisasi MPU6050
-  if (!mpu.begin(0x68, &I2C_MPU)) {
+  if (!mpu.begin(0x68, &Wire)) {
     Serial.println("MPU6050 tidak terdeteksi!");
-    lcd.setCursor(0, 0);
+    lcd.setCursor(0, 1);
     lcd.print("MPU ERROR!");
     while (1);
   }
 
-  // Atur rentang sensitivitas
-  mpu.setAccelerometerRange(MPU6050_RANGE_2_G); // paling sensitif
+  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
   mpu.setGyroRange(MPU6050_RANGE_250_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
-  // Inisialisasi buzzer
   pinMode(buzzerPin, OUTPUT);
   digitalWrite(buzzerPin, LOW);
 
-  Serial.println("MPU6050 Siap!");
-  lcd.setCursor(0, 0);
-  lcd.print("MPU6050 Siap!");
-  delay(1000);
+  delay(1500);
   lcd.clear();
 }
 
@@ -59,23 +43,17 @@ void loop() {
   sensors_event_t accel, gyro, temp;
   mpu.getEvent(&accel, &gyro, &temp);
 
-  // Hitung magnitudo getaran (tanpa arah)
   float magnitude = sqrt(accel.acceleration.x * accel.acceleration.x +
                          accel.acceleration.y * accel.acceleration.y +
                          accel.acceleration.z * accel.acceleration.z) - 9.81;
-
-  // Konversi ke nilai absolut (m/s²)
   if (magnitude < 0) magnitude = -magnitude;
 
-  // Konversi ke G-force
   float magnitude_g = magnitude / 9.81;
 
-  // Cetak ke Serial Monitor
   Serial.print("Getaran: ");
   Serial.print(magnitude_g, 3);
   Serial.println(" g");
 
-  // Tampilkan ke LCD
   lcd.setCursor(0, 0);
   lcd.print("Getaran: ");
   lcd.print(magnitude_g, 2);
@@ -91,11 +69,6 @@ void loop() {
   else
     lcd.print("Getaran Kuat!!! ");
 
-  // Kontrol buzzer
-  if (magnitude_g > 0.6)
-    digitalWrite(buzzerPin, HIGH);
-  else
-    digitalWrite(buzzerPin, LOW);
-
+  digitalWrite(buzzerPin, magnitude_g > 0.6 ? HIGH : LOW);
   delay(200);
 }
